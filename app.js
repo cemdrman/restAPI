9f982bd15c80e3699b872106450ada26fbb296eb
cem dırman <cemdrman@gmail.com>
2018-01-01 23:27:22 +0300

    error fixed

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var express = require("express"),
     port = process.env.port || 3000,
     boydParser = require("body-parser");
 
+    app.use(boydParser.json());
+    app.use(boydParser.urlencoded({extended:true}));
 
-
+    app.get("/",function(req,res){
+        res.json(
+            {message:'test api'}
+        );
+    });
 
 app.listen(port,function(){
     console.log("application is runnig on " + port);
